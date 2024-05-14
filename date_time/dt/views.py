b32836a55a18dfe61a2d2939aa3f65fdc9101478
Vigneshwaran M <116196353+Vigneshwaran-18@users.noreply.github.com>
2024-05-15 00:58:40 +0530

    Update views.py

--- date_time/dt/views.py
+++ date_time/dt/views.py
@@ -5,15 +5,15 @@ from django.http import HttpResponse
 # Create your views here.
 def curr_dt(request):
     now=datetime.datetime.now()
-    html="<html><body><h1>It is now %s</h1></body></html>"%now
+    html="<html><body><h1>It is now %s</h1></body><a href=\"https://github.com/Vigneshwaran-18\">Multiplication Table</a></html>"%now
     return HttpResponse(html)
 
 def fha(request):
     now=datetime.datetime.now() + datetime.timedelta(hours=+4)
-    html="<html><body><h1>It is now %s</h1></body></html>"%now
+    html="<html><body><h1>It is now %s</h1></body><a href=\"https://github.com/Vigneshwaran-18\">Multiplication Table</a></html>"%now
     return HttpResponse(html)
 
 def fhb(request):
     now=datetime.datetime.now() + datetime.timedelta(hours=-4)
-    html="<html><body><h1>It is now %s</h1></body></html>"%now
+    html="<html><body><h1>It is now %s</h1></body><a href=\"https://github.com/Vigneshwaran-18\">Multiplication Table</a></html>"%now
     return HttpResponse(html)

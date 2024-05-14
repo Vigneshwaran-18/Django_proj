f05e8cdf2790180d42a77848dcebd9069e365541
Vigneshwaran M <116196353+Vigneshwaran-18@users.noreply.github.com>
2024-05-15 00:29:27 +0530

    Add files via upload

---
+++ date_time/dt/views.py
@@ -0,0 +1,19 @@
+import datetime
+from django.http import HttpResponse
+#from django.shortcuts import render
+
+# Create your views here.
+def curr_dt(request):
+    now=datetime.datetime.now()
+    html="<html><body><h1>It is now %s</h1></body></html>"%now
+    return HttpResponse(html)
+
+def fha(request):
+    now=datetime.datetime.now() + datetime.timedelta(hours=+4)
+    html="<html><body><h1>It is now %s</h1></body></html>"%now
+    return HttpResponse(html)
+
+def fhb(request):
+    now=datetime.datetime.now() + datetime.timedelta(hours=-4)
+    html="<html><body><h1>It is now %s</h1></body></html>"%now
+    return HttpResponse(html)
